a82e208e6fae481b4581fe5e51364ef3f47e3beb
beliasm <143860132+beliasm@users.noreply.github.com>
2023-09-02 10:59:33 +0700

    Add files via upload
    upload file

---
+++ latihan1.py
@@ -0,0 +1,22 @@
+nama_saya = str(input("masukan nama kamu = "))
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = int(input("masukkan umur kamu = "))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input("masukan IPK kamu = "))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input("sudah menikah? = "))
+print("menikah?", menikah, "bertipe", type(menikah))
+
+# matkul_diambil = ["alpro", "sig"]
+# print("Matkul : ",matkul_diambil, "bertipe", type(matkul_diambil))
+
+# matkul_diambil2 = ["web", "pemvis"]
+# print("Matkul Lain: ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+# matkul_diambil3 = ["Alpro", "ALpro", "PenWeb", "Data Science", "Data Science"]
+# print("Matkul Lain-Lain: ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+ 
